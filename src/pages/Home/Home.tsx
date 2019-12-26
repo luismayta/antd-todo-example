dd94f0cb97c2bbc082f41e7762bf8326edc954c7
@slovacus <slovacus@gmail.com>
2019-12-26 18:39:03 -0500

    (#7) refactor: component todo list

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,12 @@
+import { Layout, Typography } from 'antd';
 import * as React from 'react';
 import ClearTodoForm from '../../container/forms/clearTodoForm';
 import CreateTodoForm from '../../container/forms/createTodoForm';
 import RemoveTodoForm from '../../container/forms/removeTodoForm';
 import SaveTodoForm from '../../container/forms/saveTodoForm';
 import TodoList from '../../container/todoList';
+const {Title, Paragraph} = Typography
+const {Header,Content} = Layout
 
 interface IProps {
     //
@@ -17,32 +20,31 @@ export class Home extends React.Component<IProps, IState> {
 
     public render() {
         return (
-            <div>
+          <React.Fragment>
+            <Layout>
+              <Typography>
+              <Header>
+                  <Title>
+                    Sample Todo App
+                  </Title>
+                  <Paragraph>
+                    Using React Framework.
+                  </Paragraph>
+              </Header>
 
-                <header>
-                    <h1><i className="nes-logo brand" /> Sample ToDo App</h1>
-                    <p>Using React Framework.</p>
-                </header>
-
-                <section className="form nes-container with-title">
-                    <h2 className="title">Form</h2>
-                    <div className="nes-field">
+              <Content>
+                  <Title level={2}>
+                    Form
+                  </Title>
                         <CreateTodoForm />
-                    </div>
-                    <div className="nes-field is-inline">
                         <RemoveTodoForm />
                         <ClearTodoForm />
                         <SaveTodoForm />
-                    </div>
-                </section>
-
-                <section className="nes-container with-title">
-                    <h2 className="title">TodoList</h2>
-                    <div className="field">
-                        <TodoList />
-                    </div>
-                </section>
-            </div>
+                <TodoList />
+              </Content>
+              </Typography>
+            </Layout>
+          </React.Fragment>
         );
     }
 }
